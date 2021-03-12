136a459e09a06919e4faa8219e615b993d122cb9
Anna Garcia <anna.garcia@beyondidentity.com>
2021-03-12 10:02:46 -0500

    Initial Commit

---
+++ authenticator-ios-sdk/authenticator_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  authenticator_ios_sdk.h
+//  authenticator-ios-sdk
+//
+//  Created by Anna Garcia on 3/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for authenticator_ios_sdk.
+FOUNDATION_EXPORT double authenticator_ios_sdkVersionNumber;
+
+//! Project version string for authenticator_ios_sdk.
+FOUNDATION_EXPORT const unsigned char authenticator_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <authenticator_ios_sdk/PublicHeader.h>
+
+
---
+++ authenticator-ios-sdkTests/authenticator_ios_sdkTests.swift
@@ -0,0 +1,33 @@
+//
+//  authenticator_ios_sdkTests.swift
+//  authenticator-ios-sdkTests
+//
+//  Created by Anna Garcia on 3/12/21.
+//
+
+import XCTest
+@testable import authenticator_ios_sdk
+
+class authenticator_ios_sdkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
